47295f06a2bff129315048e864979bfcf0392588
Howard Liu <hwd.liu@gmail.com>
2019-10-09 10:16:27 +1100

    Updates

---
+++ precache-manifest.21a692a98ce9e976e2d854d63ca2abc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "154a1b613956a4d6855a",
+    "url": "/me/static/js/main.154a1b61.chunk.js"
+  },
+  {
+    "revision": "34e00957aea25a1fb4c3",
+    "url": "/me/static/js/2.34e00957.chunk.js"
+  },
+  {
+    "revision": "154a1b613956a4d6855a",
+    "url": "/me/static/css/main.81fc10b6.chunk.css"
+  },
+  {
+    "revision": "e3687d3c523e2a2f9cf77b7697fb8878",
+    "url": "/me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.b5385446eecc00f4a9b188084a88cdaa.js"
+  "/me/precache-manifest.21a692a98ce9e976e2d854d63ca2abc7.js"
 );
 
 workbox.clientsClaim();
